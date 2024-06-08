dc4102828b98d4826f9a6d629f57261513438fb1
forsaken <forsaken@shadow.lan>
2024-06-08 09:33:09 -0400

    removed: middleware prevented only api call

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,11 @@
 // middleware.ts
-import { verifyRequestOrigin } from "lucia";
+
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { validateRequest } from "./lib/auth/validate-request";
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
   // const { user } = await validateRequest();
-  console.log(request.ip);
-  if (request.method === "GET") {
-    return NextResponse.next();
-  }
-  const originHeader = request.headers.get("Origin");
-  const hostHeader = request.headers.get("Host");
-  if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader])) {
-    return new NextResponse(null, {
-      status: 403,
-    });
-  }
+
   return NextResponse.next();
 }
 
